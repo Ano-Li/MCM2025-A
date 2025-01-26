d66bb7153afe1d89c42d4970fcca27368889ffd5
GDjellyF <gdjellies@gmail.com>
2025-01-26 19:15:29 +0800

    GDF
    Q1.1v2

--- MCM2025Q1_1.m
+++ MCM2025Q1_1.m
@@ -1,21 +1,22 @@
+%体积
 clear
 
-k_vals = 0.03;  % Range of possible wear coefficients
+k_vals = 0.05;  % Range of possible wear coefficients
 H      = 5e10;  % Hardness [Pa]
 Fn     = 686;    % wight [N]
 s0     = 0.01;  % Slip distance per footstep [m]
 years_used = 500;
 
-V_measured = [0.220205004748652	0.204164538696960	0.158817616410066...
-    0.110766429072162	0.0935579954313017	0.128500231316975];   % Volume of wear [m^3]
+W_measured = [0.00344070319919769	0.00319007091714000	0.00248152525640727...
+    0.00173072545425254	0.00146184367861409	0.00200781611432773];   % Volume of wear [m^3]
 
 % RESULTS STORAGE
-f_steps_array = zeros(size(V_measured));
+f_steps_array = zeros(size(W_measured));
 
-for i = 1:length(V_measured)
-    V_current = V_measured(i);
+for i = 1:length(W_measured)
+    W_current = W_measured(i);
     % Archardns law => s = V * H / (k * Fn)
-    s_total = (V_current * H) / (k_vals * Fn);
+    s_total = (W_current * H) / (k_vals * Fn);
     
     % Number of footfalls
     f_steps = s_total / s0;
@@ -27,5 +28,39 @@ f_steps_per_year = f_steps_array / years_used;
 
 % Display some results
 disp('Possible usage rates (footfalls/year) for various wear coefficients:');
-disp(table(V_measured', f_steps_per_year', 'VariableNames', ...
+disp(table(W_measured', f_steps_per_year', 'VariableNames', ...
+           {'W', 'f'}));
+
+%%
+%深度
+clear
+
+k_vals = 0.05;  % Range of possible wear coefficients
+H      = 5e10;  % Hardness [Pa]
+Pn     = 30000;    % wight [Pa]
+s0     = 0.01;  % Slip distance per footstep [m]
+years_used = 500;
+
+W_measured = [2.75494166405037	2.61586063889118	1.75092293274812...
+    3.53323352844741	1.25984857145743	2.95858067693070];   % depth of wear [cm]
+
+% RESULTS STORAGE
+f_steps_array = zeros(size(W_measured));
+
+for i = 1:length(W_measured)
+    W_current = W_measured(i);
+    % Archardns law => s = W * H / (k * Pn)
+    s_total = (W_current * H) / (k_vals * Pn);
+    
+    % Number of footfalls
+    f_steps = s_total / s0;
+    f_steps_array(i) = f_steps;
+end
+
+% Convert to usage per year (assuming continuous usage over 300 years)
+f_steps_per_year = f_steps_array / years_used;
+
+% Display some results
+disp('Possible usage rates (footfalls/year) for various wear coefficients:');
+disp(table(W_measured', f_steps_per_year', 'VariableNames', ...
            {'W', 'f'}));
--- MCM2025ST_W.m
+++ MCM2025ST_W.m
@@ -1,5 +1,6 @@
+clear
 W_all = []; % 初始化磨损体积数组
-
+W_maxdepth = [];
 for idx = 1:6
     % 拼接文件名并加载
     file_name = ['old-stairs-1_stairs-0', num2str(idx), '.mat'];
@@ -7,10 +8,12 @@ for idx = 1:6
 
     % 归一化 Wear 数据
     Wear = Wear - min(Wear, [], 'all'); % 将 Wear 的最小值调整为 0
-
+    
+    Wmax = max(max(Wear));
     % 计算磨损体积
-    W_measured = sum(Wear, 'all') * 4^3 * 1e-9; % [m^3]
+    W_measured = sum(Wear, 'all') * 1e-9; % [m^3]
 
     % 记录磨损体积
+    W_maxdepth = [W_maxdepth Wmax];
     W_all = [W_all W_measured]; % 将计算结果存入数组
 end
